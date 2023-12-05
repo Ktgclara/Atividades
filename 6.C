c493f3a6cb470e0879111f3d5aa35b92cc3d8491
Maria Clara <151687335+Ktgclara@users.noreply.github.com>
2023-12-04 21:40:38 -0300

    Create 6.C

---
+++ 6.C
@@ -0,0 +1,10 @@
+int main() {
+ float num1, num2; 
+ printf("digite o primeiro numero");
+ scanf("%f", &num1);
+ printf("digite o segundo numero");
+ scanf("%f", &num2); 
+ divisao = num1 / num2;
+ printf("a divisao e: %f", divisao);
+    return 0;
+}

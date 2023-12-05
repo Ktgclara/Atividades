75f9aad26161ba81d130e5f18b75a40571fc9726
Maria Clara <151687335+Ktgclara@users.noreply.github.com>
2023-12-04 21:41:40 -0300

    Create 8.C

---
+++ 8.C
@@ -0,0 +1,10 @@
+int main() {
+  float raio, area, comprimento;
+  printf("digite o raio da circunferencia");
+  scanf("%f", &raio);
+  area = 3,14 * raio * raio;
+  comprimento = 2 * 3,14 * raio;
+  printf("A area da circunferencia e: %f", area);
+  printf("O comprimento da circunferencia e: %f", comprimento);
+  return 0;
+}
